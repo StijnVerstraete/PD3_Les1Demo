bd1e024aa3b469590858f648255f9f1b36c60833
StijnVerstraete <43540802+StijnVerstraete@users.noreply.github.com>
2018-09-24 15:45:41 +0200

    Adding Initial Scene Setup

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour {
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+}
